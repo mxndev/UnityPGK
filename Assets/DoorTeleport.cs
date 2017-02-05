9d23e7f17cc5845403d213993994a33e8c39db2c
Mikołaj Płachta <mixxenxd@wp.pl>
2017-02-05 17:21:16 +0100

    Ukończenie poziomu pierwszego.

--- Assets/DoorTeleport.cs
+++ Assets/DoorTeleport.cs
@@ -4,25 +4,36 @@ using UnityEngine;
 using UnityStandardAssets.Characters.FirstPerson;
 
 public class DoorTeleport : MonoBehaviour {
-	Vector3 playerPos;
 	float offset;
 	FirstPersonController player;
 
 	// Use this for initialization
 	void Start () {
-		playerPos = new Vector3 ();
-		offset = 50.0f;
+		offset = 0.8f;
 		player = (FirstPersonController)FindObjectOfType(typeof(FirstPersonController));
 	}
-}
 
 
 	// Update is called once per frame
-	void Update () {
-		playerPos = player.transform.position;
-		if(player.transform.position )
+	void Update () 
+	{
+		if (CheckPlayerInDoors(player.transform.position, this.transform.position))
+		{
+			float x = Mathf.Round (this.transform.position.x);
+			float z = Mathf.Round (this.transform.position.z);
+			if ((Mathf.Round(this.transform.position.x) == 124) && (Mathf.Round(this.transform.position.z) == 221)) {
+				player.transform.position = new Vector3 (100.0f, 0.0f, 214.0f);
+			} else {
+				player.transform.position = new Vector3 (35.0f, 0.0f, 214.0f);
+			}
+		}
+	}
 	bool CheckPlayerInDoors(Vector3 playerPos, Vector3 doorPos)
 	{
-		if(((doorPos.x - playerPos.x <= offset) || (playerPos.x - doorPos.x >= offset)) && ((doorPos.y - playerPos.y <= offset) || (playerPos.x - doorPos.x >= offset)))
+		if((Mathf.Abs(doorPos.x - playerPos.x) <= offset) && (Mathf.Abs(doorPos.z - playerPos.z) <= offset))
+		{
+			return true;
+		}
+		return false;
 	}
 }
